ca859f4a9ae4cba4c27ca783cfc2ed8952341ca9
c8e4 <c8e4@proton.me>
2024-03-21 17:53:14 +0100

    zero dependency html widget

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,12 @@
     import * as srust from "ergo-lib-wasm-browser";
     import { Network } from "@fleet-sdk/core";
 
+    // html widget
+    import hljs from 'highlight.js/lib/core';
+    import scala from 'highlight.js/lib/languages/scala';
+    hljs.registerLanguage('scala', scala);
+    // html widget
+
     import {
     Address,
     BlockHeaders,
@@ -191,6 +197,151 @@
         createLobyWithId(lobbyId, true);
     }
 
+    function copyErgoScriptAsHtml(){
+        const codeString = hljs.highlight('scala', editor.getValue()).value;
+        navigator.clipboard.writeText(`
+<style>
+.hljs {
+  color: #ddd;
+}
+
+.hljs-tag,
+.hljs-keyword,
+.hljs-selector-tag,
+.hljs-literal,
+.hljs-strong,
+.hljs-name {
+  color: #f92672;
+}
+
+.hljs-code {
+  color: #66d9ef;
+}
+
+.hljs-attribute,
+.hljs-symbol,
+.hljs-regexp,
+.hljs-link {
+  color: #bf79db;
+}
+
+.hljs-string,
+.hljs-bullet,
+.hljs-subst,
+.hljs-title,
+.hljs-section,
+.hljs-emphasis,
+.hljs-type,
+.hljs-built_in,
+.hljs-selector-attr,
+.hljs-selector-pseudo,
+.hljs-addition,
+.hljs-variable,
+.hljs-template-tag,
+.hljs-template-variable {
+  color: #a6e22e;
+}
+
+.hljs-title.class_,
+.hljs-class .hljs-title {
+  color: white;
+}
+
+.hljs-comment,
+.hljs-quote,
+.hljs-deletion,
+.hljs-meta {
+  color: #75715e;
+}
+
+.hljs-keyword,
+.hljs-selector-tag,
+.hljs-literal,
+.hljs-doctag,
+.hljs-title,
+.hljs-section,
+.hljs-type,
+.hljs-selector-id {
+  font-weight: bold;
+}
+
+.ios-buttons {
+    display: flex;
+    gap: 5px;
+    padding: 0.7em;
+    padding-left: 1em;
+}
+
+.ios-button {
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    background-color: #fff;
+    cursor: pointer;
+}
+
+.ios-button.close {
+    background-color: #ff3b30;
+}
+
+.ios-button.minimize {
+    background-color: #ffcc00;
+}
+
+.ios-button.maximize {
+    background-color: #4cd964;
+}
+
+.fake-window-header {
+    border-top-left-radius: 6px;
+    border-top-right-radius: 6px;
+    position: sticky;
+    top: 0;
+    background-color: #1e1e1e;
+    width:100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: gray;
+    font-family: monospace;
+}
+
+.editor-flex {
+    background: #1e1e1e;
+    position: relative;
+    height: 300px;
+    width: 600px;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    overflow: scroll;
+    resize: both;
+    -webkit-box-shadow: 0px 0px 81px 5px rgba(0, 0, 0, 0.7);
+    -moz-box-shadow: 0px 0px 81px 5px rgba(0, 0, 0, 0.7);
+    box-shadow: 0px 0px 81px 5px rgba(0, 0, 0, 0.7);
+}
+.source-code{
+    margin-left:16px;
+}
+</style>
+
+
+<div class="editor-flex">
+    <div class="fake-window-header">
+                <div class="ios-buttons">
+                    <div class="ios-button close" />
+                    <div class="ios-button minimize" />
+                    <div class="ios-button maximize" />
+                </div>
+                <div style="padding-right:8px;">ergoscript</div>
+            </div>
+            <div class="source-code">
+                <pre><code class="hljs">${codeString}</code></pre>
+            </div>   
+    </div>
+</div>
+    `);
+    }
+
     function createBookmark() {
         if (window.sidebar && window.sidebar.addPanel) {
             // Firefox
@@ -333,6 +484,25 @@
             {/if}
 
             <!-- <Avatars /> -->
+            <button
+                on:click={copyErgoScriptAsHtml}
+                class="flex items-center px-2 py-1 gray-border"
+                style="border-style:dashed;"
+            >
+                copy as html
+                <svg 
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                class="ml-2">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M3.46447 3.46447C2 4.92893 2 7.28595 2 12C2 16.714 2 19.0711 3.46447 20.5355C4.92893 22 7.28595 22 12 22C16.714 22 19.0711 22 20.5355 20.5355C22 19.0711 22 16.714 22 12C22 7.28595 22 4.92893 20.5355 3.46447C19.0711 2 16.714 2 12 2C7.28595 2 4.92893 2 3.46447 3.46447ZM13.4881 6.44591C13.8882 6.55311 14.1256 6.96437 14.0184 7.36447L11.4302 17.0237C11.323 17.4238 10.9117 17.6613 10.5116 17.5541C10.1115 17.4468 9.8741 17.0356 9.98131 16.6355L12.5695 6.97624C12.6767 6.57614 13.088 6.3387 13.4881 6.44591ZM14.9697 8.46967C15.2626 8.17678 15.7374 8.17678 16.0303 8.46967L16.2387 8.67801C16.874 9.3133 17.4038 9.84308 17.7678 10.3202C18.1521 10.8238 18.4216 11.3559 18.4216 12C18.4216 12.6441 18.1521 13.1762 17.7678 13.6798C17.4038 14.1569 16.874 14.6867 16.2387 15.322L16.0303 15.5303C15.7374 15.8232 15.2626 15.8232 14.9697 15.5303C14.6768 15.2374 14.6768 14.7626 14.9697 14.4697L15.1412 14.2981C15.8229 13.6164 16.2797 13.1574 16.5753 12.7699C16.8577 12.3998 16.9216 12.1843 16.9216 12C16.9216 11.8157 16.8577 11.6002 16.5753 11.2301C16.2797 10.8426 15.8229 10.3836 15.1412 9.70191L14.9697 9.53033C14.6768 9.23744 14.6768 8.76257 14.9697 8.46967ZM7.96986 8.46967C8.26275 8.17678 8.73762 8.17678 9.03052 8.46967C9.32341 8.76257 9.32341 9.23744 9.03052 9.53033L8.85894 9.70191C8.17729 10.3836 7.72052 10.8426 7.42488 11.2301C7.14245 11.6002 7.07861 11.8157 7.07861 12C7.07861 12.1843 7.14245 12.3998 7.42488 12.7699C7.72052 13.1574 8.17729 13.6164 8.85894 14.2981L9.03052 14.4697C9.32341 14.7626 9.32341 15.2374 9.03052 15.5303C8.73762 15.8232 8.26275 15.8232 7.96986 15.5303L7.76151 15.322C7.12617 14.6867 6.59638 14.1569 6.23235 13.6798C5.84811 13.1762 5.57861 12.6441 5.57861 12C5.57861 11.3559 5.84811 10.8238 6.23235 10.3202C6.59638 9.84308 7.12617 9.31331 7.76151 8.67801L7.96986 8.46967Z" 
+                    fill="#bdbdbd"
+                    />
+                    </svg>
+            </button>
+
             <button
                 on:click={toggleFlex}
                 class="flex items-center px-2 py-1 gray-border"
