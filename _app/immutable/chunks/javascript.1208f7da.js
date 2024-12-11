858e7f311fe93935dfd830f641a079071ac0d958
c8e4 <c8e4@proton.me>
2024-12-11 18:37:36 +0100

    Updates

--- _app/immutable/chunks/javascript.1208f7da.js
+++ _app/immutable/chunks/javascript.1208f7da.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript.fdc436b7.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript.f0f1c22a.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.44.0(3e047efd345ff102c8c61b5398fb30845aaac166)
  * Released under the MIT license
